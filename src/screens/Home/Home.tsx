086b4178d3e069202876c0df641c6098bd32f57f
carlainha <160603557+carlainha@users.noreply.github.com>
2025-04-23 12:23:04 -0300

    DIA 23/04

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -3,6 +3,8 @@ import { View, Image, Text, ScrollView, ImageBackground } from 'react-native';
 import Fundo from '../../../assets/fundoHome.png';
 import { styles } from './HomeStyles';
 import { Pesquisar } from '../../components/TextInput/Pesquisar';
+import FontAwesome6 from '@react-native-vector-icons/fontawesome6';
+
 
 
 export function Home() {
@@ -11,7 +13,7 @@ export function Home() {
             <View style={styles.container}>
                 <ImageBackground source={Fundo} style={styles.imagem}/>
                 <View style={styles.icones}>
-                    <Text style={styles.icone1}>V</Text>
+                    <FontAwesome6 name="comments" />
                     <Text style={styles.icone2}>V</Text>
                 </View>
                 <Pesquisar/>
