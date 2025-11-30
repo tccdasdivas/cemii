6c04d8863c5e5a7af1251b1395b4901eb7fb72ca
carlainha <160603557+carlainha@users.noreply.github.com>
2025-11-29 22:41:46 -0300

    Delete src/components/TextInput/Input.tsx

--- src/components/TextInput/Input.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { TextInput, TextInputProps } from 'react-native';
-import { styles } from './InputStyles';
-
-// Aqui o componente herda TODAS as props do TextInput nativo
-export function Input({ ...rest }: TextInputProps) {
-  return <TextInput style={styles.input} {...rest} />;
-}
